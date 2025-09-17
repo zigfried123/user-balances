a5daba29bfa9fab04981a2216044fff733529852
zigfried123 <zigfried123@mail.ru>
2025-09-17 13:16:34 +0400

    fix

--- app/Services/OperationService.php
+++ app/Services/OperationService.php
@@ -2,11 +2,18 @@
 
 namespace App\Services;
 
+use App\Models\Balance;
 use Exception;
 
 class OperationService
 {
-    public function changeBalanceByType($balance, $data) {
+    /**
+     * @param Balance $balance
+     * @param array $data Содержит тип операции и сумму
+     * @return void
+     * @throws Exception
+     */
+    public function changeBalanceByType(Balance $balance, array $data) {
         if($data['type'] === 'credit') {
             $balance->total += $data['sum'];
         }elseif($data['type'] === 'debit' && $balance->total >= $data['sum']){
